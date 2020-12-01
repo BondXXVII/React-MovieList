aea0d54e4beffd19b01b1b02970339f8a11b0c71
Bond <Bond@email>
2020-12-01 01:47:34 -0500

    added popcorn, first 2 trailers, notes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import './App.css';
 import ReactPlayer from 'react-player';
 import HmmHmHmm from './images/mordocoffee.png';
+// import Popcorn from './images/popcorn.png';
 
 class App extends React.Component {
   constructor(props) {
@@ -17,14 +18,11 @@ class App extends React.Component {
 
   componentDidMount() {this.loadMovies();}
 
-  loadMovies() {
-    // go grab (aka get) all the data from some url
+  loadMovies() {    // go grab (aka get) all the data from some url
     const url = "https://api.themoviedb.org/3/discover/movie?api_key=b6fbc7f3f313bd395902af464ef47262&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
     axios.get(url)
-      .then(response =>
-      {
-        // then put just the movies (not everything) into the state
-        console.log(response.data.results)
+      .then(response => {   // then put just the movies (not everything) into the state
+        console.log(response.data.results) //comment out after studying array in log
         this.setState({movies: response.data.results});
       });
   }
@@ -37,46 +35,26 @@ class App extends React.Component {
 
   bootClick = () => {
     const trailer = 'https://www.youtube.com/watch?v=';
-    let vidLink = 'qHFssXdRGQM';
+    let vidLink = 'qHFssXdRGQM';  //need to find out where 'data-id' is located in API<<<<<<
+    // let vidLink = '7Y6-w5Psupw';
     this.setState({theVid: (trailer + vidLink), bigScreen: '', bigTitle: ''})
   }
 
-  render() {
-    const imP = 'https://image.tmdb.org/t/p/w500';
-    // const trailer = 'https://www.youtube.com/watch?v=';
+  render() {    //need to put rating and loop genre<<<<<
+    const imPath = 'https://image.tmdb.org/t/p/w500';
     return (
       <div className="App">
         <img className="bgImage" src='https://www.searchpng.com/wp-content/uploads/2019/01/Theater-Clipart-Png-Image.png' alt=''/>
         <button className='bootn' onClick={this.bootClick}>Play Trailer</button>
         <div className='screen'><ReactPlayer className='player' url={this.state.theVid}/><h4>{this.state.bigTitle} <br/><br/> {this.state.bigScreen}</h4><img style={{width: '13em'}} src={HmmHmHmm} alt=''/></div>
-        {this.state.movies.map((movie) =>
-          {
-            return <div key={movie.title}><img className="imgs" src={imP + movie.poster_path} alt={movie.overview} id={movie.title} onClick={this.handleClick}/><h5 className="tiles"> {movie.title} <br/> ({movie.vote_average}/10)</h5></div>;
-          })}
+        {this.state.movies.map((movie) => {
+            return <div key={movie.title}><img className="imgs" src={imPath + movie.poster_path} alt={movie.overview} id={movie.title} onClick={this.handleClick}/><h5 className="tiles"> {movie.title} <br/> ({movie.vote_average}/10)</h5></div>;
+          })
+        }
+        {/* <img className="zoom" src={Popcorn} alt=''/> */}
       </div>
     );
   }
 }
-
-//   render() {
-//     // let movies = [];
-//     // for(let i = 0; i < this.state.movies.length; i++)
-//     // {
-//     //   movies.push(<h2>{this.state.movies[i].title}</h2>)
-//     // }
-//     const imP = "https://image.tmdb.org/t/p/w500";
-//     return (
-//       <div className="App">
-//         <img className="imag" src='https://st2.depositphotos.com/1000868/7740/v/950/depositphotos_77408874-stock-illustration-movie-hall-with-red-seats.jpg' />
-//         <h1>Check out these movies!!!!!!</h1>
-//         {this.state.movies.map(movie =>
-//           {
-//             return <h2><img className="imgs" src={imP + movie.poster_path}/> {movie.title} ({movie.release_date})</h2>;
-            
-//           })}
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
